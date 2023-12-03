54e7336038e836af9f0ccc1507c067205587969e
PacifistBluey <121199062+PacifistBluey@users.noreply.github.com>
2023-12-03 11:06:20 -0500

    Registry Part 1

---
+++ Assets/FourSwordTubers.cs
@@ -0,0 +1,16 @@
+using WeaverCore;
+
+public class FourSwordTubers : WeaverMod
+{
+    public FourSwordTubers() : base("Four-Sword Tubers") { }
+
+    public override string GetVersion()
+    {
+        return "1.0";
+    }
+
+    public override void Initialize()
+    {
+        
+    }
+}
